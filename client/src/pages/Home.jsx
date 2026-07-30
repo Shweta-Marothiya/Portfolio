e1fc8eb3b15843683a48edb6d559aa5d472c2aef
Shweta Marothiya <shwetamarothiya2011@gmail.com>
2026-07-30 18:58:43 +0530

    Remove Footer from Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,6 @@ import Skills from "../components/Skills/Skills";
 import Projects from "../components/Projects/Projects";
 import Education from "../components/Education/Education";
 import Contact from "../components/Contact/Contact";
-import Footer from "../components/Footer/Footer";
 
 function Home() {
     return (
@@ -17,7 +16,6 @@ function Home() {
             <Projects />
             <Education />
             <Contact />
-            <Footer />
         </div>
     );
 }
